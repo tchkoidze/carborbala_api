22dd567f4b6af533abdda3293ac1613752c3eaf7
tchkoidze <ilia.chkoidze12@gmail.com>
2024-03-12 15:57:19 +0400

    feat: create controller for cars

---
+++ src/controllers/carController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import Car from "models/Cars";
+
+export const getAllCars = async (_: Request, res: Response) => {
+  try {
+    const data = await Car.find();
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(401).json(error);
+  }
+};
