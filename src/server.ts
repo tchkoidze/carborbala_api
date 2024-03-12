725d481670d51cbc8533651270c8337a1c4be2d4
tchkoidze <ilia.chkoidze12@gmail.com>
2024-03-12 17:34:42 +0400

    add swager for get car request

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connect from "./config/mongo.js";
 import carRouter from "./routes/car-router.js";
+import swaggerMiddleware from "middlewares/swagger-middleware.js";
 
 dotenv.config();
 console.log(process.env.MONGO_URL);
@@ -15,5 +16,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/api", carRouter);
+
+app.use("/", ...swaggerMiddleware);
 
 app.listen(process.env.Port || 3000);
