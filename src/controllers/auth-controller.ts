4b713097479ea39e77305c8cda7dba5320309c6a
tchkoidze <ilia.chkoidze12@gmail.com>
2024-03-15 11:50:14 +0400

    create login function, at auth -controller file

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,17 +1,50 @@
 import { Request, Response } from "express";
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const createUser = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
 
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const newUser = new User({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
     newUser.save();
     return res.status(200).json(newUser);
+  } catch (error) {
+    return res.status(401).json(error);
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email }, { _id: 0, _v: 0 }).select(
+      "+password"
+    );
+
+    if (!user) {
+      return res.status(4001).json("User with that email doesn`t exist");
+    }
+
+    const result = await bcrypt.compare(password, user.password);
+
+    if (result) {
+      const sightData = {
+        name: user.name,
+        id: user.id,
+      };
+
+      const token = jwt.sign(sightData, process.env.JWT_SECRET!);
+
+      return res.status(200).json({ ...sightData, token });
+    }
   } catch (error) {
     return res.status(401).json(error);
   }
