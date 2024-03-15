8015533cfd11406a9a9f6669a39c37bd9534734a
tchkoidze <ilia.chkoidze12@gmail.com>
2024-03-15 11:55:53 +0400

    add login at swagger

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -1,7 +1,8 @@
-import { createUser } from "../controllers/auth-controller.js";
+import { createUser, login } from "../controllers/auth-controller.js";
 import express from "express";
 
 const authRouter = express.Router();
 authRouter.post("/register", createUser);
+authRouter.post("/login", login);
 
 export default authRouter;
